27feb8a2d37976f4ff514f5eca831af19a09bfc7
aort93 <alonsojortega@gmail.com>
2019-04-24 17:18:54 -0400

    almost done

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,38 @@ class App extends React.Component {
     }
   }
 
+// why the friends doesn't a constant work here
+  fetchUrl = () => { return (this.state.filters.type === 'all') ? '/api/pets' : `/api/pets?type=` + this.state.filters.type}
+
+  onChangeType = (event) => {
+    this.setState({filters: {type: event.target.value}})
+  }
+
+  onAdoptPet = (event) => {
+    console.log(event.target.id)
+    const pet = this.state.pets.find(pet=> pet.id=== event.target.id)
+    pet.isAdopted = true
+    const index = this.state.pets.findIndex(pet => pet.id === event.target.id)
+    const pets = this.state.pets
+    pets[index] = pet
+    this.setState({pets: pets})
+
+    // const newPet =  {...pet, isAdopted: true}
+    // setState([...this.state.pets], {...pet}])
+
+
+
+    // this.setState({})
+  }
+
+  onFindPetsClick = (event) => {
+    fetch(this.fetchUrl())
+    .then(response => response.json())
+    .then(myJson => this.setState({pets: myJson}))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +55,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets}/>
             </div>
           </div>
         </div>
